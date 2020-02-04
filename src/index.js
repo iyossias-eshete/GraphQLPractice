afacadd3546f56fe5b1c88fad17500c6297c60bf
Iyossias Eshete <iyossias777@gmail.com>
2020-02-04 11:57:47 +0300

    Intial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const typeDefs = `
+    type Query{
+        info: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        info : ()=> null
+       // info : ()=>`This is the API of a Hackernews Clone`
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(( ) => console.log(`Server is running on http://localhost:4000`));
